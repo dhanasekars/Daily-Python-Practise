772aa44af1290f3d0b4171d1b27f765adb4c4c33
DS <dhanasekars@gmail.com>
2022-08-05 20:52:25 +0530

    daily check in

---
+++ 2022-08-04.py
@@ -0,0 +1,29 @@
+""" 
+Created on : 04/08/22 6:06 PM
+@author : ds  
+"""
+
+
+def online_count(statuses):
+    count = 0
+    for key,value in statuses.items():
+        if value == 'online':
+            count = count + 1
+    return count
+
+
+
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+    "Alice3": "online",
+    "Bob3": "offline",
+    "Eve3": "online",
+}
+
+
+
+
+print(online_count(statuses=statuses))
---
+++ 2022-08-05.py
@@ -0,0 +1,12 @@
+""" 
+Created on : 05/08/22 8:47 PM
+@author : ds  
+"""
+import random
+
+
+def random_number():
+    return random.randint(1, 100)
+
+
+print(random_number())
