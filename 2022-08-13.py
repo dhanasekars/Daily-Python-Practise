19bdd0da44529553fd7f460389af3077e5da039e
DS <dhanasekars@gmail.com>
2022-08-14 18:36:35 +0530

    daily check in

---
+++ 2022-08-13.py
@@ -0,0 +1,11 @@
+""" 
+Created on : 13/08/22 4:36 PM
+@author : ds  
+"""
+
+
+def div_3(a):
+    return a % 3 == 0
+
+
+print(div_3(5))
---
+++ 2022-08-14.py
@@ -0,0 +1,18 @@
+"""
+Created on : 14/08/22 6:27 PM
+@author : ds  
+"""
+
+
+def get_row_col(s):
+    translate = {"A":0, "B":1, "C":2}
+    letter = s[0]
+    number = s[1]
+    row = int(number) - 1
+    column = translate[letter]
+    return row, column
+
+
+
+
+print(get_row_col("A3"))
