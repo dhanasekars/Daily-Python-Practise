2d224f011f98efda4fba99b6bad7fc2f0aaaee4f
DS <dhanasekars@gmail.com>
2022-11-26 15:32:15 +0530

    Midday checkin solving for Nov 25.

--- 2022-11/Nov_24.py
+++ 2022-11/Nov_24.py
@@ -18,36 +18,3 @@ class Testpaper:
         self.subject = subject
         self.markscheme = markscheme
         self.pass_mark = float(pass_mark.strip('%'))
-
-
-class Student:
-    """
-    student class for students to take tests.
-    """
-    marks_d = {}
-
-    def __init__(self, tests_taken="None"):
-        self.tests_taken = "No tests taken" if tests_taken == "None" else tests_taken
-
-    # For Nov 24
-
-    def take_test(self, paper, answers):
-        if len(paper.markscheme) == len(answers):
-            correct_answers = [i for i, j in zip(paper.markscheme, answers) if i == j]
-            percent_scored = len(correct_answers) / len(answers) * 100
-            if percent_scored <= paper.pass_mark:
-                self.marks_d = f'Passed! ({int(percent_scored)}%'
-            else:
-                self.marks_d[paper.subject] = f'Failed! ({int(percent_scored)}%)'
-                self.tests_taken = self.marks_d
-        return self.tests_taken
-
-
-paper1 = Testpaper("Maths", ["1A", "2C", "3D", "4A", "5A"], "60%")
-paper2 = Testpaper("Physics", ["1B", "2A", "3D", "4B", "5C"], "80%")
-
-student1 = Student()
-
-student1.take_test(paper1, ["1A", "2C", "3D", "4B", "5A"])
-student1.take_test(paper2, ["1B", "2B", "3D", "4B", "5A"])
-print(student1.tests_taken)
---
+++ Playground/test_file_for_student_test.py
@@ -0,0 +1,43 @@
+""" 
+Created on : 26/11/22 2:31 PM
+@author : ds  
+"""
+
+from student_test import Testpaper, Student
+import unittest
+
+
+class test_Testpaper(unittest.TestCase):
+
+    def test_subject(self):
+        paper1 = Testpaper("Maths", ["1A", "2C", "3D", "4A", "5A"], "60%")
+        self.assertEqual(paper1.subject,"Maths")
+
+    def test_markscheme(self):
+        paper1 = Testpaper("Maths", ["1A", "2C", "3D", "4A", "5A"], "60%")
+        self.assertEqual(paper1.markscheme,["1A", "2C", "3D", "4A", "5A"])
+
+    def test_pass_mark(self):
+        paper1 = Testpaper("Maths", ["1A", "2C", "3D", "4A", "5A"], "60%")
+        self.assertEqual(paper1.pass_mark, "60%")
+
+    def test_no_tests_taken(self):
+        student1 = Student()
+        self.assertEqual(student1.tests_taken,"No tests taken")
+
+    def test_student_tests_taken(self):
+        paper1 = Testpaper("Maths", ["1A", "2C", "3D", "4A", "5A"], "60%")
+        paper2 = Testpaper("Chemistry", ["1C", "2C", "3D", "4A"], "75%")
+        paper3 = Testpaper("Computing", ["1D", "2C", "3C", "4B", "5D", "6C", "7A"], "75%")
+        student1 = Student()
+        student2 = Student()
+        self.assertEqual(student1.tests_taken,"No tests taken")
+        student1.take_test(paper1, ["1A", "2D", "3D", "4A", "5A"])
+        self.assertEqual(student1.tests_taken, {"Maths": "Passed! (80%)"})
+        student2.take_test(paper2, ["1C", "2D", "3A", "4C"])
+        student2.take_test(paper3, ["1A", "2C", "3A", "4C", "5D", "6C", "7B"])
+        self.assertEqual(student2.tests_taken, {"Chemistry": "Failed! (25%)", "Computing": "Failed! (42%)"})
+
+
+
+
