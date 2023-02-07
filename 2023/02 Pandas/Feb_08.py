2bacf467a7ab9c62f26556d0eaf82d2a0d32284b
DS <dhanasekars@gmail.com>
2023-02-08 05:23:24 +0530

    Daily check in - Pandas. No unittests

---
+++ 2023/02 Pandas/Feb_08.py
@@ -0,0 +1,15 @@
+""" 
+Created on : 08/02/23 5:12 AM
+@author : ds  
+"""
+
+
+def feb_29_count(df):
+    """
+    How many employees were hired on February 29th?
+    :param df:
+    :return: integer
+    """
+    return df.loc[(df.hire_date.dt.day == 29) & (df.hire_date.dt.month == 2)].shape[0]
+
+
