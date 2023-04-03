52f6ff0d582cd3c2dac71618bfe746f18e12bfc8
DS <dhanasekars@gmail.com>
2023-04-03 10:28:41 +0530

    daily checkin

---
+++ 2023/04 /03.py
@@ -0,0 +1,21 @@
+""" 
+Created on : 03/04/23 10:08 am
+@author : ds  
+"""
+# http://www.pythonchallenge.com/pc/def/equality.html
+
+
+import re
+
+# Open the text file
+with open('03_data.txt', 'r') as file:
+    text = file.read()
+
+pattern = r"[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+"
+
+matches = re.findall(pattern, text)
+code_word = ""
+print("".join(matches))
+
+# linkedlist
+# Level 04 http://www.pythonchallenge.com/pc/def/linkedlist.php
