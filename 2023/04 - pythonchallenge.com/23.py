41e4269e653b5c3f8fc773c27a6b3f1dba17a438
DS <dhanasekars@gmail.com>
2023-04-23 19:00:26 +0530

    daily checkin

---
+++ 2023/04 - pythonchallenge.com/23.py
@@ -0,0 +1,12 @@
+""" 
+Created on : 23/04/23 6:52 pm
+@author : ds  
+"""
+import codecs
+
+s = 'va gur snpr bs jung?'
+print(codecs.decode(s,"rot-13"))
+
+#  in the face of what?
+
+#  http://www.pythonchallenge.com/pc/hex/ambiguity.html
