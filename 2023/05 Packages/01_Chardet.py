c21d6fc8c8ef7520ae117f3c385014e48acc072f
DS <dhanasekars@gmail.com>
2023-05-01 12:06:23 +0530

    daily checkin

--- 2023/05 Packages/01_Chardet.py
+++ 2023/05 Packages/01_Chardet.py
@@ -3,3 +3,19 @@ Created on : 21/04/23 3:02 pm
 @author : ds  
 """
 import chardet
+import urllib.request
+from chardet.universaldetector import UniversalDetector
+
+# helps to find the encoding type used
+
+rawdata = urllib.request.urlopen('http://mixi.jp')
+detector = UniversalDetector()
+for line in rawdata.readlines():
+    detector.feed(line)
+    if detector.done:
+        break
+detector.close()
+rawdata.close()
+print(detector.result)
+
+# https://chardet.readthedocs.io/en/latest/index.html
