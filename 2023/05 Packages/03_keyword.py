d3c9a92d5c0f2a865dca5c5bf4c8a81e1e187599
DS <dhanasekars@gmail.com>
2023-05-03 12:02:55 +0530

    daily checkin

--- 2023/05 Packages/03_keyword.py
+++ 2023/05 Packages/03_keyword.py
@@ -2,3 +2,9 @@
 Created on : 27/04/23 7:11 am
 @author : ds  
 """
+import keyword
+
+for kw in keyword.kwlist:
+    print(kw)
+
+print(keyword.iskeyword('try'))
---
+++ 2023/05 Packages/12_Langchain.py
@@ -0,0 +1,4 @@
+""" 
+Created on : 02/05/23 4:27 pm
+@author : ds  
+"""
