f1ce5c2e1fda4be7b64dd3fa70d2eb8880b38b53
DS <dhanasekars@gmail.com>
2023-05-27 17:55:04 +0530

    daily checkin

--- 2023/05 Packages/27_gramformer.py
+++ 2023/05 Packages/27_gramformer.py
@@ -1,4 +1,10 @@
 """ 
 Created on : 24/05/23 1:32 pm
-@author : ds  
+@author : ds
 """
+
+import gramformer
+
+gf = gramformer.Gramformer(models=1, use_gpu=False)
+output = gf.correct(""" New Zealand is island countrys in southwestern Paciific Ocaen. Country population was 5 million """)
+print(output)
