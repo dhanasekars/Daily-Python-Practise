dbb53334a2fee256fc26f3d077d5639274f748b4
DS <dhanasekars@gmail.com>
2023-05-30 16:36:41 +0530

    daily checkin

---
+++ 2023/05 Packages/30_getpass.py
@@ -0,0 +1,14 @@
+""" 
+Created on : 30/05/23 4:26 pm
+@author : ds  
+"""
+
+import getpass
+
+
+print(getpass.getpass("Password:"))
+
+print(getpass.__all__)
+getpass.GetPassWarning("This is a warning message to print.")
+print(getpass.getuser())
+
