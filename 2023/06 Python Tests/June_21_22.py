e68ededa2f1eea88969755e95365dcaab427d828
DS <dhanasekars@gmail.com>
2023-06-22 05:31:05 +0530

    daily checkin

--- 2023/06 Python Tests/June_21_22.py
+++ 2023/06 Python Tests/June_21_22.py
@@ -8,15 +8,23 @@ using pyscopg2.
 As of June 21, 2023 there is no active posgres installed locally, need to revisit this in the coming weekend
 after having an active postgresql server running.
 
+A cloud Postgres was setup in a DBaaS (elephantSQL) and a connection was established on June 22, 2023
+
 """
 
 import psycopg2
+from config import Postgresql
 
-conn = psycopg2.connect(host, port, database, user, password )
+conn = psycopg2.connect(
+    host=Postgresql.elephantsql_url, 
+    port=Postgresql.elephantsql_port,
+    database=Postgresql.database,
+    user=Postgresql.user,
+    password=Postgresql.password )
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM test_table")
+cursor.execute("SELECT * FROM COMPANY;")
 rows = cursor.fetchall()
 
 for row in rows:
