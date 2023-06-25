34838ede27900e7063f5ac0222b903ab759734f8
DS <dhanasekars@gmail.com>
2023-06-25 13:10:31 +0530

    daily push

---
+++ 2023/06 Python Tests/June_25.py
@@ -0,0 +1,9 @@
+""" 
+Created on : 25/06/23 1:05 pm
+@author : ds  
+"""
+
+#  Just for fun executing js file from Python :-)
+from Naked.toolshed.shell import execute_js, muterun_js
+
+print(execute_js('path-example.js'))
