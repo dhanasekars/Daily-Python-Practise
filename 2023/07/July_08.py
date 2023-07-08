04e109af406d5ab8a2033d040e4120ba4df2c17e
DS <dhanasekars@gmail.com>
2023-07-08 09:15:05 +0530

    daily practise

---
+++ 2023/07/July_08.py
@@ -0,0 +1,18 @@
+""" 
+Created on : 08/07/23 9:12 am
+@author : ds  
+"""
+test_data = [
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 0],
+  [0, 1, 0, 0]
+]
+
+
+def tallest_skyscraper(lst):
+    return sum(1 in row for row in lst)
+
+
+print(tallest_skyscraper(test_data))
+
