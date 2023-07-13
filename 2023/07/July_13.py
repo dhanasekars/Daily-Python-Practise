ff53ad72276a90a894d1f60f8b2e942fa8da8c17
DS <dhanasekars@gmail.com>
2023-07-13 19:00:34 +0530

    daily practise

---
+++ 2023/07/July_13.py
@@ -0,0 +1,22 @@
+""" 
+Created on : 13/07/23 6:58 pm
+@author : ds  
+"""
+
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+
+    return wrapper
+
+
+def say_whee():
+    print("Whee!")
+
+
+say_whee = my_decorator(say_whee)
+
+say_whee()
