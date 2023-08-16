28e678f34e22bb189110935e95071f0c32a7e8f5
DS <dhanasekars@gmail.com>
2023-08-16 06:17:20 +0530

    updated requirements

---
+++ 2023/08 graphQL/Aug_15_04.py
@@ -0,0 +1,4 @@
+""" 
+Created on : 15/08/23 12:37 pm
+@author : ds  
+"""
---
+++ 2023/08 graphQL/Aug_15_streamlit.py
@@ -0,0 +1,10 @@
+""" 
+Created on : 15/08/23 9:58 am
+@author : ds  
+"""
+
+import streamlit as st
+
+
+st.write("""### ```My first app Hello ``` """)
+st.button('Hit me')
